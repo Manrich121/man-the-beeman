2376e57aaaab2d86c475d866d24689a87197a3a8
Manrich <manrich@journeyapps.com>
2020-02-21 21:23:30 +0200

    Updates

---
+++ precache-manifest.4d130c70b929168e7690b4d74356a8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eec483b49646302c7fe404e1ed7b546",
+    "url": "/man-the-beeman/index.html"
+  },
+  {
+    "revision": "b61ca8cdc8789a2c5bb1",
+    "url": "/man-the-beeman/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e0b4c1c8205858844c3",
+    "url": "/man-the-beeman/static/js/2.52b0316b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/man-the-beeman/static/js/2.52b0316b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61ca8cdc8789a2c5bb1",
+    "url": "/man-the-beeman/static/js/main.44157a27.chunk.js"
+  },
+  {
+    "revision": "031c016ae087f993d5dd",
+    "url": "/man-the-beeman/static/js/runtime-main.ee976480.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/man-the-beeman/static/media/logo.5d5d9eef.svg"
+  }
+]);
